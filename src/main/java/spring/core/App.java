37d98921f3764d6117fdd9f731ceeb955a2d768b
marchenkoandy <marchenkoandy@gmail.com>
2017-12-06 02:34:12 +0200

    Commit homework

--- src/main/java/spring/core/App.java
+++ src/main/java/spring/core/App.java
@@ -1,8 +1,9 @@
 package spring.core;
 
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import spring.core.beans.Client;
-import spring.core.loggers.ConsoleEventLogger;
 import spring.core.loggers.EventLogger;
 
 public class App {
@@ -11,22 +12,24 @@ public class App {
     private Client client;
     private EventLogger eventLogger;
 
-    public void logEvent(String msg){
-        String message = msg.replaceAll(client.getId(),client.getFullName());
+    public void logEvent(String msg) {
+        String message = msg.replaceAll(client.getId(), client.getFullName());
         eventLogger.logEvent(message);
     }
 
-    public App(Client client, EventLogger eventLogger){
+    public App(Client client, EventLogger eventLogger) {
         this.client = client;
         this.eventLogger = eventLogger;
     }
 
     public static void main(String[] args) {
-//        App app = new App();
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("Schema.xml");
+        App app = (App) ctx.getBean(App.class);
 //        app.client = new Client("1","John Smith");
 //        app.eventLogger = new ConsoleEventLogger();
 ////
-//        app.logEvent("Some event for user 1");
+        app.logEvent("Some event for user 1");
+        app.logEvent("Some event for user 2");
     }
 
 }
