522e888d0d276c24be04e6ef1ef751e6c9a6b91a
Andrii_Marchenko1 <Andrii_Marchenko1@epam.com>
2017-12-08 13:39:55 +0200

    Lesson 1.5

--- src/main/java/spring/core/loggers/CacheFileLogger.java
+++ src/main/java/spring/core/loggers/CacheFileLogger.java
@@ -2,12 +2,14 @@ package spring.core.loggers;
 
 import spring.core.beans.Event;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class CacheFileLogger extends FileEventLogger {
 
     private int cacheSize;
-    private List<Event> cache;
+    private List<Event> cache = new ArrayList<Event>();
 
     public CacheFileLogger(String fileName, int cacheSize) {
         super(fileName);
@@ -17,10 +19,9 @@ public class CacheFileLogger extends FileEventLogger {
     @Override
     public void logEvent(Event event) {
         cache.add(event);
-
         if(cache.size()==cacheSize){
-
+            writeEventsToFile(cache);
+            cache.clear();
         }
-        super.logEvent(event);
     }
 }
